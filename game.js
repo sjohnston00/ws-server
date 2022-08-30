0d9d78c8c01490f73ae4f83983545d337d38888b
Sam Johnston <johnstonsam712@gmail.com>
2022-08-30 20:08:06 +0100

    change server broadcasting

--- game.js
+++ game.js
@@ -1,23 +1,127 @@
-const ws = new WebSocket("ws://192.168.1.21:8080");
+const ws = new WebSocket("ws://192.168.240.1:8080")
 ws.addEventListener("open", (e) => {
-  console.log("successfull connection");
-});
+  console.log("successfull connection")
+})
 ws.addEventListener("message", async (e) => {
-  const data = await e.data.text();
-  console.log(data);
-});
+  const data = await e.data.text()
+  if (player === 2) {
+    if (data.startsWith("player2")) return
 
-(() => {
-  const rows = [...document.querySelectorAll(".row")];
-  const row1Cells = [...rows[0].children];
-  const player1 = rows.at(-1).children[0];
-  const player2 = row1Cells.at(-1);
-  player1.classList.add("player1");
-  player2.classList.add("player2");
-})();
+    const key = data.split("-")[1]
+
+    const playerClassName = `player1`
+
+    const activeSquare = document.querySelector(`.cell.${playerClassName}`)
+
+    if (key === "ArrowLeft") {
+      if (!activeSquare.previousElementSibling) return //we're at the left most side
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.previousElementSibling.classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
+      return
+    }
+
+    if (key === "ArrowRight") {
+      if (!activeSquare.nextElementSibling) return //we're at the right most side
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.nextElementSibling.classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
+
+      return
+    }
+
+    if (key === "ArrowUp") {
+      if (!activeSquare.parentNode.previousElementSibling) return //we're at the top
+
+      const indexOfNode = getChildElementIndex(activeSquare)
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.parentNode.previousElementSibling.children[
+        indexOfNode
+      ].classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
+
+      return
+    }
+
+    if (key === "ArrowDown") {
+      if (!activeSquare.parentNode.nextElementSibling) return //we're at the bottom
+      const indexOfNode = getChildElementIndex(activeSquare)
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.parentNode.nextElementSibling.children[
+        indexOfNode
+      ].classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
+
+      return
+    }
+  } else {
+    if (data.startsWith("player1")) return
+
+    const key = data.split("-")[1]
+
+    const playerClassName = `player2`
+
+    const activeSquare = document.querySelector(`.cell.${playerClassName}`)
+
+    if (key === "ArrowLeft") {
+      if (!activeSquare.previousElementSibling) return //we're at the left most side
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.previousElementSibling.classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
+      return
+    }
+
+    if (key === "ArrowRight") {
+      if (!activeSquare.nextElementSibling) return //we're at the right most side
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.nextElementSibling.classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
+
+      return
+    }
+
+    if (key === "ArrowUp") {
+      if (!activeSquare.parentNode.previousElementSibling) return //we're at the top
+
+      const indexOfNode = getChildElementIndex(activeSquare)
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.parentNode.previousElementSibling.children[
+        indexOfNode
+      ].classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
+
+      return
+    }
+
+    if (key === "ArrowDown") {
+      if (!activeSquare.parentNode.nextElementSibling) return //we're at the bottom
+      const indexOfNode = getChildElementIndex(activeSquare)
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.parentNode.nextElementSibling.children[
+        indexOfNode
+      ].classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
+
+      return
+    }
+  }
+})
+
+const player = Number(
+  Object.fromEntries(new URL(window.location.href).searchParams).player
+)
+
+;(() => {
+  const rows = [...document.querySelectorAll(".row")]
+  const row1Cells = [...rows[0].children]
+  const player1 = rows.at(-1).children[0]
+  const player2 = row1Cells.at(-1)
+  player1.classList.add("player1")
+  player2.classList.add("player2")
+})()
 
 document.body.addEventListener("keydown", (e) => {
-  const { key } = e;
+  const { key } = e
 
   if (
     key !== "ArrowLeft" &&
@@ -25,53 +129,56 @@ document.body.addEventListener("keydown", (e) => {
     key !== "ArrowDown" &&
     key !== "ArrowUp"
   )
-    return;
+    return
 
-  const activeSquare = document.querySelector(".cell.player1");
+  const playerNumber = player || 1
+  const playerClassName = `player${[playerNumber]}`
+
+  const activeSquare = document.querySelector(`.cell.${playerClassName}`)
 
   if (key === "ArrowLeft") {
-    if (!activeSquare.previousElementSibling) return; //we're at the left most side
-    activeSquare.classList.remove("player1");
-    activeSquare.previousElementSibling.classList.add("player1");
-    ws.send(key);
-    return;
+    if (!activeSquare.previousElementSibling) return //we're at the left most side
+    activeSquare.classList.remove(playerClassName)
+    activeSquare.previousElementSibling.classList.add(playerClassName)
+    ws.send(`${playerClassName}-${key}`)
+    return
   }
 
   if (key === "ArrowRight") {
-    if (!activeSquare.nextElementSibling) return; //we're at the right most side
-    activeSquare.classList.remove("player1");
-    activeSquare.nextElementSibling.classList.add("player1");
-    ws.send(key);
+    if (!activeSquare.nextElementSibling) return //we're at the right most side
+    activeSquare.classList.remove(playerClassName)
+    activeSquare.nextElementSibling.classList.add(playerClassName)
+    ws.send(`${playerClassName}-${key}`)
 
-    return;
+    return
   }
 
   if (key === "ArrowUp") {
-    if (!activeSquare.parentNode.previousElementSibling) return; //we're at the top
+    if (!activeSquare.parentNode.previousElementSibling) return //we're at the top
 
-    const indexOfNode = getChildElementIndex(activeSquare);
-    activeSquare.classList.remove("player1");
+    const indexOfNode = getChildElementIndex(activeSquare)
+    activeSquare.classList.remove(playerClassName)
     activeSquare.parentNode.previousElementSibling.children[
       indexOfNode
-    ].classList.add("player1");
-    ws.send(key);
+    ].classList.add(playerClassName)
+    ws.send(`${playerClassName}-${key}`)
 
-    return;
+    return
   }
 
   if (key === "ArrowDown") {
-    if (!activeSquare.parentNode.nextElementSibling) return; //we're at the bottom
-    const indexOfNode = getChildElementIndex(activeSquare);
-    activeSquare.classList.remove("player1");
+    if (!activeSquare.parentNode.nextElementSibling) return //we're at the bottom
+    const indexOfNode = getChildElementIndex(activeSquare)
+    activeSquare.classList.remove(playerClassName)
     activeSquare.parentNode.nextElementSibling.children[
       indexOfNode
-    ].classList.add("player1");
-    ws.send(key);
+    ].classList.add(playerClassName)
+    ws.send(`${playerClassName}-${key}`)
 
-    return;
+    return
   }
-});
+})
 
 function getChildElementIndex(node) {
-  return Array.prototype.indexOf.call(node.parentNode.children, node);
+  return Array.prototype.indexOf.call(node.parentNode.children, node)
 }
--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-import { WebSocketServer } from "ws";
-import ip from "ip";
+import { WebSocketServer, WebSocket } from "ws"
+import ip from "ip"
 
-const PORT = 8080;
-const IP_ADDRESS = ip.address();
-const wss = new WebSocketServer({ port: PORT });
+const PORT = 8080
+const IP_ADDRESS = ip.address()
+const wss = new WebSocketServer({ port: PORT })
 
 wss.on("connection", function connection(ws, req) {
-  const ip = req.socket.remoteAddress;
-  console.log("connection from", ip);
+  const ip = req.socket.remoteAddress
+  console.log("connection from", ip)
   ws.on("message", function message(data) {
-    ws.send(data);
-  });
-});
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data)
+      }
+    })
+  })
+})
 
 wss.on("listening", function connection(ws, req) {
   console.log(
     `web socket server listening on ws://localhost:${PORT} ws://${IP_ADDRESS}:${PORT}`
-  );
-});
+  )
+})
