23dc9fad53bea716ce69cb9912bd09e75b44f4ae
Sam Johnston <johnstonsam712@gmail.com>
2022-09-02 09:46:20 +0100

    make list of my ips

--- scripts/game.js
+++ scripts/game.js
@@ -1,130 +1,131 @@
-const ws = new WebSocket("ws://192.168.1.21:8080");
-const progress = document.querySelector("progress");
-const createLobbyBtn = document.getElementById("create-lobby");
-const joinLobbyBtn = document.getElementById("join-lobby");
+const myIPs = ["192.168.1.21", "192.168.1.82"]
+const ws = new WebSocket(`ws://${myIPs[1]}:8080`)
+const progress = document.querySelector("progress")
+const createLobbyBtn = document.getElementById("create-lobby")
+const joinLobbyBtn = document.getElementById("join-lobby")
 ws.addEventListener("open", (e) => {
-  console.log("successfull connection");
-});
+  console.log("successfull connection")
+})
 ws.addEventListener("message", async (e) => {
-  const data = await e.data.text();
+  const data = await e.data.text()
   if (player === 2) {
-    if (data.startsWith("player2")) return;
+    if (data.startsWith("player2")) return
 
-    const key = data.split("-")[1];
+    const key = data.split("-")[1]
 
-    const playerClassName = `player1`;
+    const playerClassName = `player1`
 
-    const activeSquare = document.querySelector(`.cell.${playerClassName}`);
+    const activeSquare = document.querySelector(`.cell.${playerClassName}`)
 
     if (key === "ArrowLeft") {
-      if (!activeSquare.previousElementSibling) return; //we're at the left most side
-      activeSquare.classList.remove(playerClassName);
-      activeSquare.previousElementSibling.classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
-      return;
+      if (!activeSquare.previousElementSibling) return //we're at the left most side
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.previousElementSibling.classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
+      return
     }
 
     if (key === "ArrowRight") {
-      if (!activeSquare.nextElementSibling) return; //we're at the right most side
-      activeSquare.classList.remove(playerClassName);
-      activeSquare.nextElementSibling.classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
+      if (!activeSquare.nextElementSibling) return //we're at the right most side
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.nextElementSibling.classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
 
-      return;
+      return
     }
 
     if (key === "ArrowUp") {
-      if (!activeSquare.parentNode.previousElementSibling) return; //we're at the top
+      if (!activeSquare.parentNode.previousElementSibling) return //we're at the top
 
-      const indexOfNode = getChildElementIndex(activeSquare);
-      activeSquare.classList.remove(playerClassName);
+      const indexOfNode = getChildElementIndex(activeSquare)
+      activeSquare.classList.remove(playerClassName)
       activeSquare.parentNode.previousElementSibling.children[
         indexOfNode
-      ].classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
+      ].classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
 
-      return;
+      return
     }
 
     if (key === "ArrowDown") {
-      if (!activeSquare.parentNode.nextElementSibling) return; //we're at the bottom
-      const indexOfNode = getChildElementIndex(activeSquare);
-      activeSquare.classList.remove(playerClassName);
+      if (!activeSquare.parentNode.nextElementSibling) return //we're at the bottom
+      const indexOfNode = getChildElementIndex(activeSquare)
+      activeSquare.classList.remove(playerClassName)
       activeSquare.parentNode.nextElementSibling.children[
         indexOfNode
-      ].classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
+      ].classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
 
-      return;
+      return
     }
   } else {
-    if (data.startsWith("player1")) return;
+    if (data.startsWith("player1")) return
 
-    const key = data.split("-")[1];
+    const key = data.split("-")[1]
 
-    const playerClassName = `player2`;
+    const playerClassName = `player2`
 
-    const activeSquare = document.querySelector(`.cell.${playerClassName}`);
+    const activeSquare = document.querySelector(`.cell.${playerClassName}`)
 
     if (key === "ArrowLeft") {
-      if (!activeSquare.previousElementSibling) return; //we're at the left most side
-      activeSquare.classList.remove(playerClassName);
-      activeSquare.previousElementSibling.classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
-      return;
+      if (!activeSquare.previousElementSibling) return //we're at the left most side
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.previousElementSibling.classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
+      return
     }
 
     if (key === "ArrowRight") {
-      if (!activeSquare.nextElementSibling) return; //we're at the right most side
-      activeSquare.classList.remove(playerClassName);
-      activeSquare.nextElementSibling.classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
+      if (!activeSquare.nextElementSibling) return //we're at the right most side
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.nextElementSibling.classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
 
-      return;
+      return
     }
 
     if (key === "ArrowUp") {
-      if (!activeSquare.parentNode.previousElementSibling) return; //we're at the top
+      if (!activeSquare.parentNode.previousElementSibling) return //we're at the top
 
-      const indexOfNode = getChildElementIndex(activeSquare);
-      activeSquare.classList.remove(playerClassName);
+      const indexOfNode = getChildElementIndex(activeSquare)
+      activeSquare.classList.remove(playerClassName)
       activeSquare.parentNode.previousElementSibling.children[
         indexOfNode
-      ].classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
+      ].classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
 
-      return;
+      return
     }
 
     if (key === "ArrowDown") {
-      if (!activeSquare.parentNode.nextElementSibling) return; //we're at the bottom
-      const indexOfNode = getChildElementIndex(activeSquare);
-      activeSquare.classList.remove(playerClassName);
+      if (!activeSquare.parentNode.nextElementSibling) return //we're at the bottom
+      const indexOfNode = getChildElementIndex(activeSquare)
+      activeSquare.classList.remove(playerClassName)
       activeSquare.parentNode.nextElementSibling.children[
         indexOfNode
-      ].classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
+      ].classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
 
-      return;
+      return
     }
   }
-});
+})
 
 const player = Number(
   Object.fromEntries(new URL(window.location.href).searchParams).player
-);
+)
 
-(() => {
-  const rows = [...document.querySelectorAll(".row")];
-  const row1Cells = [...rows[0].children];
-  const player1 = rows.at(-1).children[0];
-  const player2 = row1Cells.at(-1);
-  player1.classList.add("player1");
-  player2.classList.add("player2");
-})();
+;(() => {
+  const rows = [...document.querySelectorAll(".row")]
+  const row1Cells = [...rows[0].children]
+  const player1 = rows.at(-1).children[0]
+  const player2 = row1Cells.at(-1)
+  player1.classList.add("player1")
+  player2.classList.add("player2")
+})()
 
 document.body.addEventListener("keydown", (e) => {
-  const { key } = e;
+  const { key } = e
 
   if (
     key !== "ArrowLeft" &&
@@ -132,74 +133,74 @@ document.body.addEventListener("keydown", (e) => {
     key !== "ArrowDown" &&
     key !== "ArrowUp"
   )
-    return;
+    return
 
-  const playerNumber = player || 1;
-  const playerClassName = `player${[playerNumber]}`;
+  const playerNumber = player || 1
+  const playerClassName = `player${[playerNumber]}`
 
-  const activeSquare = document.querySelector(`.cell.${playerClassName}`);
+  const activeSquare = document.querySelector(`.cell.${playerClassName}`)
 
   if (key === "ArrowLeft") {
-    if (!activeSquare.previousElementSibling) return; //we're at the left most side
-    activeSquare.classList.remove(playerClassName);
-    activeSquare.previousElementSibling.classList.add(playerClassName);
-    ws.send(`${playerClassName}-${key}`);
-    return;
+    if (!activeSquare.previousElementSibling) return //we're at the left most side
+    activeSquare.classList.remove(playerClassName)
+    activeSquare.previousElementSibling.classList.add(playerClassName)
+    ws.send(`${playerClassName}-${key}`)
+    return
   }
 
   if (key === "ArrowRight") {
-    if (!activeSquare.nextElementSibling) return; //we're at the right most side
-    activeSquare.classList.remove(playerClassName);
-    activeSquare.nextElementSibling.classList.add(playerClassName);
-    ws.send(`${playerClassName}-${key}`);
+    if (!activeSquare.nextElementSibling) return //we're at the right most side
+    activeSquare.classList.remove(playerClassName)
+    activeSquare.nextElementSibling.classList.add(playerClassName)
+    ws.send(`${playerClassName}-${key}`)
 
-    return;
+    return
   }
 
   if (key === "ArrowUp") {
-    if (!activeSquare.parentNode.previousElementSibling) return; //we're at the top
+    if (!activeSquare.parentNode.previousElementSibling) return //we're at the top
 
-    const indexOfNode = getChildElementIndex(activeSquare);
-    activeSquare.classList.remove(playerClassName);
+    const indexOfNode = getChildElementIndex(activeSquare)
+    activeSquare.classList.remove(playerClassName)
     activeSquare.parentNode.previousElementSibling.children[
       indexOfNode
-    ].classList.add(playerClassName);
-    ws.send(`${playerClassName}-${key}`);
+    ].classList.add(playerClassName)
+    ws.send(`${playerClassName}-${key}`)
 
-    return;
+    return
   }
 
   if (key === "ArrowDown") {
-    if (!activeSquare.parentNode.nextElementSibling) return; //we're at the bottom
-    const indexOfNode = getChildElementIndex(activeSquare);
-    activeSquare.classList.remove(playerClassName);
+    if (!activeSquare.parentNode.nextElementSibling) return //we're at the bottom
+    const indexOfNode = getChildElementIndex(activeSquare)
+    activeSquare.classList.remove(playerClassName)
     activeSquare.parentNode.nextElementSibling.children[
       indexOfNode
-    ].classList.add(playerClassName);
-    ws.send(`${playerClassName}-${key}`);
+    ].classList.add(playerClassName)
+    ws.send(`${playerClassName}-${key}`)
 
-    return;
+    return
   }
-});
+})
 
 createLobbyBtn.addEventListener("click", () => {
   const data = JSON.stringify({
     type: "create-lobby",
-    data: null
-  });
-  ws.send(data);
-});
+    data: null,
+  })
+  ws.send(data)
+})
 joinLobbyBtn.addEventListener("click", () => {
-  const lobbyId = prompt("Enter lobby id");
+  const lobbyId = prompt("Enter lobby id")
   const data = JSON.stringify({
     type: "join-lobby",
     data: {
-      lobbyId
-    }
-  });
-  ws.send(data);
-});
+      lobbyId,
+    },
+  })
+  ws.send(data)
+})
 
 function getChildElementIndex(node) {
-  return Array.prototype.indexOf.call(node.parentNode.children, node);
+  return Array.prototype.indexOf.call(node.parentNode.children, node)
 }
