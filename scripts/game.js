a25c30e2b58579eab6cd5e4d537ff0c2ab21b2e3
Sam Johnston <johnstonsam712@gmail.com>
2022-09-12 12:55:01 +0100

    join lobbies using dialog list

--- scripts/game.js
+++ scripts/game.js
@@ -1,178 +1,178 @@
 //@ts-nocheck
-const myIPs = ["192.168.1.21", "192.168.1.82", "192.168.240.1"];
-const ws = new WebSocket(`ws://${myIPs[0]}:8080`);
-const progress = document.querySelector("progress");
-const createLobbyBtn = document.getElementById("create-lobby");
-const joinLobbyBtn = document.getElementById("join-lobby");
-const exitLobbyBtn = document.getElementById("exit-lobby");
-const viewAllLobbiesBtn = document.getElementById("view-all-lobbies");
-const lobbyIdInput = document.getElementById("lobby-id");
-const lobbyInfo = document.getElementById("lobby-info");
-const wsInfo = document.getElementById("ws-info");
-const lobbiesDialog = document.getElementById("lobbies-dialog");
-const lobbiesDiv = document.getElementById("lobbies");
-const closeDialogBtn = document.getElementById("close-dialog");
-let currentLobby;
+const myIPs = ["192.168.1.21", "192.168.1.82", "192.168.240.1", "localhost"]
+const ws = new WebSocket(`ws://${myIPs[3]}:8080`)
+const progress = document.querySelector("progress")
+const createLobbyBtn = document.getElementById("create-lobby")
+const joinLobbyBtn = document.getElementById("join-lobby")
+const exitLobbyBtn = document.getElementById("exit-lobby")
+const viewAllLobbiesBtn = document.getElementById("view-all-lobbies")
+const lobbyIdInput = document.getElementById("lobby-id")
+const lobbyInfo = document.getElementById("lobby-info")
+const wsInfo = document.getElementById("ws-info")
+const lobbiesDialog = document.getElementById("lobbies-dialog")
+const lobbiesDiv = document.getElementById("lobbies")
+const closeDialogBtn = document.getElementById("close-dialog")
+let currentLobby
 ws.addEventListener("open", (e) => {
-  updateWsInfo({ ...e, connected: true });
-});
+  updateWsInfo({ ...e, connected: true })
+})
 ws.addEventListener("message", async (e) => {
   // updateWsInfo()
 
-  const data = e.data;
-  const json = JSON.parse(data);
+  const data = e.data
+  const json = JSON.parse(data)
 
   // console.log(json)
   // console.log(currentLobby)
 
   if (!currentLobby && json.type === "create-lobby") {
-    currentLobby = json.data.lobby;
-    createLobby(currentLobby);
-    return;
+    currentLobby = json.data.lobby
+    createLobby(currentLobby)
+    return
   }
   if (!currentLobby && json.type === "join-lobby") {
-    currentLobby = json.data.lobby;
-    joinLobby(currentLobby);
-    return;
+    currentLobby = json.data.lobby
+    joinLobby(currentLobby)
+    return
   }
 
   if (currentLobby?.id === json.lobbyId && json.type === "exit-lobby") {
-    updateLobbyInfo(undefined);
-    return;
+    updateLobbyInfo(undefined)
+    return
   }
 
   if (json.type === "all-lobbies") {
-    console.log(json);
-    displayLobbies(json.data.lobbies);
-    return;
+    console.log(json)
+    displayLobbies(json.data.lobbies)
+    return
   }
 
   if (json.data?.lobby?.id === currentLobby?.id) {
-    updateLobbyInfo(json.data.lobby);
+    updateLobbyInfo(json.data.lobby)
   }
 
   if (player === 2) {
-    if (data.startsWith("player2")) return;
+    if (data.startsWith("player2")) return
 
-    const key = data.split("-")[1];
+    const key = data.split("-")[1]
 
-    const playerClassName = `player1`;
+    const playerClassName = `player1`
 
-    const activeSquare = document.querySelector(`.cell.${playerClassName}`);
+    const activeSquare = document.querySelector(`.cell.${playerClassName}`)
 
     if (key === "ArrowLeft") {
-      if (!activeSquare.previousElementSibling) return; //we're at the left most side
-      activeSquare.classList.remove(playerClassName);
-      activeSquare.previousElementSibling.classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
-      return;
+      if (!activeSquare.previousElementSibling) return //we're at the left most side
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.previousElementSibling.classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
+      return
     }
 
     if (key === "ArrowRight") {
-      if (!activeSquare.nextElementSibling) return; //we're at the right most side
-      activeSquare.classList.remove(playerClassName);
-      activeSquare.nextElementSibling.classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
+      if (!activeSquare.nextElementSibling) return //we're at the right most side
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.nextElementSibling.classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
 
-      return;
+      return
     }
 
     if (key === "ArrowUp") {
-      if (!activeSquare.parentNode.previousElementSibling) return; //we're at the top
+      if (!activeSquare.parentNode.previousElementSibling) return //we're at the top
 
-      const indexOfNode = getChildElementIndex(activeSquare);
-      activeSquare.classList.remove(playerClassName);
+      const indexOfNode = getChildElementIndex(activeSquare)
+      activeSquare.classList.remove(playerClassName)
       activeSquare.parentNode.previousElementSibling.children[
         indexOfNode
-      ].classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
+      ].classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
 
-      return;
+      return
     }
 
     if (key === "ArrowDown") {
-      if (!activeSquare.parentNode.nextElementSibling) return; //we're at the bottom
-      const indexOfNode = getChildElementIndex(activeSquare);
-      activeSquare.classList.remove(playerClassName);
+      if (!activeSquare.parentNode.nextElementSibling) return //we're at the bottom
+      const indexOfNode = getChildElementIndex(activeSquare)
+      activeSquare.classList.remove(playerClassName)
       activeSquare.parentNode.nextElementSibling.children[
         indexOfNode
-      ].classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
+      ].classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
 
-      return;
+      return
     }
   } else {
-    if (data.startsWith("player1")) return;
+    if (data.startsWith("player1")) return
 
-    const key = data.split("-")[1];
+    const key = data.split("-")[1]
 
-    const playerClassName = `player2`;
+    const playerClassName = `player2`
 
-    const activeSquare = document.querySelector(`.cell.${playerClassName}`);
+    const activeSquare = document.querySelector(`.cell.${playerClassName}`)
 
     if (key === "ArrowLeft") {
-      if (!activeSquare.previousElementSibling) return; //we're at the left most side
-      activeSquare.classList.remove(playerClassName);
-      activeSquare.previousElementSibling.classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
-      return;
+      if (!activeSquare.previousElementSibling) return //we're at the left most side
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.previousElementSibling.classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
+      return
     }
 
     if (key === "ArrowRight") {
-      if (!activeSquare.nextElementSibling) return; //we're at the right most side
-      activeSquare.classList.remove(playerClassName);
-      activeSquare.nextElementSibling.classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
+      if (!activeSquare.nextElementSibling) return //we're at the right most side
+      activeSquare.classList.remove(playerClassName)
+      activeSquare.nextElementSibling.classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
 
-      return;
+      return
     }
 
     if (key === "ArrowUp") {
-      if (!activeSquare.parentNode.previousElementSibling) return; //we're at the top
+      if (!activeSquare.parentNode.previousElementSibling) return //we're at the top
 
-      const indexOfNode = getChildElementIndex(activeSquare);
-      activeSquare.classList.remove(playerClassName);
+      const indexOfNode = getChildElementIndex(activeSquare)
+      activeSquare.classList.remove(playerClassName)
       activeSquare.parentNode.previousElementSibling.children[
         indexOfNode
-      ].classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
+      ].classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
 
-      return;
+      return
     }
 
     if (key === "ArrowDown") {
-      if (!activeSquare.parentNode.nextElementSibling) return; //we're at the bottom
-      const indexOfNode = getChildElementIndex(activeSquare);
-      activeSquare.classList.remove(playerClassName);
+      if (!activeSquare.parentNode.nextElementSibling) return //we're at the bottom
+      const indexOfNode = getChildElementIndex(activeSquare)
+      activeSquare.classList.remove(playerClassName)
       activeSquare.parentNode.nextElementSibling.children[
         indexOfNode
-      ].classList.add(playerClassName);
-      ws.send(`${playerClassName}-${key}`);
+      ].classList.add(playerClassName)
+      ws.send(`${playerClassName}-${key}`)
 
-      return;
+      return
     }
   }
-});
+})
 
 ws.addEventListener("error", (e) => {
-  updateWsInfo({ ...e, connected: false });
-});
+  updateWsInfo({ ...e, connected: false })
+})
 
 const player = Number(
   Object.fromEntries(new URL(window.location.href).searchParams).player
-);
+)
 
-(() => {
-  const rows = [...document.querySelectorAll(".row")];
-  const row1Cells = [...rows[0].children];
-  const player1 = rows.at(-1).children[0];
-  const player2 = row1Cells.at(-1);
-  player1.classList.add("player1");
-  player2.classList.add("player2");
-})();
+;(() => {
+  const rows = [...document.querySelectorAll(".row")]
+  const row1Cells = [...rows[0].children]
+  const player1 = rows.at(-1).children[0]
+  const player2 = row1Cells.at(-1)
+  player1.classList.add("player1")
+  player2.classList.add("player2")
+})()
 
 document.body.addEventListener("keydown", (e) => {
-  const { key } = e;
+  const { key } = e
 
   if (
     key !== "ArrowLeft" &&
@@ -180,162 +180,185 @@ document.body.addEventListener("keydown", (e) => {
     key !== "ArrowDown" &&
     key !== "ArrowUp"
   )
-    return;
+    return
 
-  const playerNumber = player || 1;
-  const playerClassName = `player${[playerNumber]}`;
+  const playerNumber = player || 1
+  const playerClassName = `player${[playerNumber]}`
 
-  const activeSquare = document.querySelector(`.cell.${playerClassName}`);
+  const activeSquare = document.querySelector(`.cell.${playerClassName}`)
   let moveData = {
     type: "move",
     data: {
       lobbyId: currentLobby?.id,
-      move: key
-    }
-  };
+      move: key,
+    },
+  }
 
   if (key === "ArrowLeft") {
-    if (!activeSquare.previousElementSibling) return; //we're at the left most side
-    activeSquare.classList.remove(playerClassName);
-    activeSquare.previousElementSibling.classList.add(playerClassName);
+    if (!activeSquare.previousElementSibling) return //we're at the left most side
+    activeSquare.classList.remove(playerClassName)
+    activeSquare.previousElementSibling.classList.add(playerClassName)
     // ws.send(`${playerClassName}-${key}`)
     // return
   }
 
   if (key === "ArrowRight") {
-    if (!activeSquare.nextElementSibling) return; //we're at the right most side
-    activeSquare.classList.remove(playerClassName);
-    activeSquare.nextElementSibling.classList.add(playerClassName);
+    if (!activeSquare.nextElementSibling) return //we're at the right most side
+    activeSquare.classList.remove(playerClassName)
+    activeSquare.nextElementSibling.classList.add(playerClassName)
     // ws.send(`${playerClassName}-${key}`)
 
     // return
   }
 
   if (key === "ArrowUp") {
-    if (!activeSquare.parentNode.previousElementSibling) return; //we're at the top
+    if (!activeSquare.parentNode.previousElementSibling) return //we're at the top
 
-    const indexOfNode = getChildElementIndex(activeSquare);
-    activeSquare.classList.remove(playerClassName);
+    const indexOfNode = getChildElementIndex(activeSquare)
+    activeSquare.classList.remove(playerClassName)
     activeSquare.parentNode.previousElementSibling.children[
       indexOfNode
-    ].classList.add(playerClassName);
+    ].classList.add(playerClassName)
     // ws.send(`${playerClassName}-${key}`)
 
     // return
   }
 
   if (key === "ArrowDown") {
-    if (!activeSquare.parentNode.nextElementSibling) return; //we're at the bottom
-    const indexOfNode = getChildElementIndex(activeSquare);
-    activeSquare.classList.remove(playerClassName);
+    if (!activeSquare.parentNode.nextElementSibling) return //we're at the bottom
+    const indexOfNode = getChildElementIndex(activeSquare)
+    activeSquare.classList.remove(playerClassName)
     activeSquare.parentNode.nextElementSibling.children[
       indexOfNode
-    ].classList.add(playerClassName);
+    ].classList.add(playerClassName)
     // ws.send(`${playerClassName}-${key}`)
 
     // return
   }
 
-  ws.send(JSON.stringify(moveData));
-});
+  ws.send(JSON.stringify(moveData))
+})
 
 createLobbyBtn?.addEventListener("click", () => {
   const data = JSON.stringify({
     type: "create-lobby",
-    data: null
-  });
-  ws.send(data);
-});
+    data: null,
+  })
+  ws.send(data)
+})
 joinLobbyBtn?.addEventListener("click", () => {
-  const { value } = lobbyIdInput;
+  const { value } = lobbyIdInput
   if (!value) {
-    alert("Please enter a lobby id");
-    lobbyIdInput?.focus();
-    return;
+    alert("Please enter a lobby id")
+    lobbyIdInput?.focus()
+    return
   }
 
   const data = JSON.stringify({
     type: "join-lobby",
     data: {
-      lobbyId: value
-    }
-  });
-  ws.send(data);
-});
+      lobbyId: value,
+    },
+  })
+  ws.send(data)
+})
 
 viewAllLobbiesBtn.addEventListener("click", () => {
   const data = JSON.stringify({
     type: "all-lobbies",
-    data: null
-  });
+    data: null,
+  })
 
-  ws.send(data);
-  lobbiesDialog.showModal();
-});
+  ws.send(data)
+  lobbiesDialog.showModal()
+})
+
+lobbiesDialog.addEventListener("click", (event) => {
+  const isButton = event.target.nodeName === "BUTTON"
+  const isJoinLobbyButton = event.target.id.includes("join-lobby-")
+  if (!isButton || !isJoinLobbyButton) {
+    return
+  }
+
+  const lobbyId = event.target.id.substring(11)
+  const data = JSON.stringify({
+    type: "join-lobby",
+    data: {
+      lobbyId,
+    },
+  })
+  ws.send(data)
+  lobbiesDialog.close()
+  viewAllLobbiesBtn.style.pointerEvents = "none"
+  viewAllLobbiesBtn.style.opacity = "0"
+  clearLobbiesList()
+})
 
 closeDialogBtn.addEventListener("click", () => {
-  clearLobbiesList();
-});
+  clearLobbiesList()
+})
 
 exitLobbyBtn.addEventListener("click", () => {
-  if (!currentLobby) return;
+  if (!currentLobby) return
 
   ws.send(
     JSON.stringify({
       type: "exit-lobby",
-      lobbyId: currentLobby.id
+      lobbyId: currentLobby.id,
     })
-  );
+  )
 
-  createLobbyBtn.removeAttribute("disabled");
-  joinLobbyBtn.removeAttribute("disabled");
-  exitLobbyBtn.style.pointerEvents = "none";
-  exitLobbyBtn.style.opacity = "0";
-  lobbyIdInput.value = "";
-  lobbyIdInput.removeAttribute("disabled");
-});
+  createLobbyBtn.removeAttribute("disabled")
+  joinLobbyBtn.removeAttribute("disabled")
+  exitLobbyBtn.style.pointerEvents = "none"
+  exitLobbyBtn.style.opacity = "0"
+  viewAllLobbiesBtn.style.pointerEvents = "all"
+  viewAllLobbiesBtn.style.opacity = "1"
+  lobbyIdInput.value = ""
+  lobbyIdInput.removeAttribute("disabled")
+})
 
 function updateWsInfo(data) {
-  wsInfo.textContent = JSON.stringify(data, null, 2);
+  wsInfo.textContent = JSON.stringify(data, null, 2)
 }
 
 function getChildElementIndex(node) {
-  return Array.prototype.indexOf.call(node.parentNode.children, node);
+  return Array.prototype.indexOf.call(node.parentNode.children, node)
 }
 
 function createLobby(lobby) {
-  createLobbyBtn.setAttribute("disabled", "true");
-  joinLobbyBtn.setAttribute("disabled", "true");
-  exitLobbyBtn.style.pointerEvents = "all";
-  exitLobbyBtn.style.opacity = "1";
-  lobbyIdInput.value = "";
-  lobbyIdInput.setAttribute("disabled", "true");
-  lobbyInfo.textContent = JSON.stringify(lobby, null, 2);
+  createLobbyBtn.setAttribute("disabled", "true")
+  joinLobbyBtn.setAttribute("disabled", "true")
+  exitLobbyBtn.style.pointerEvents = "all"
+  exitLobbyBtn.style.opacity = "1"
+  lobbyIdInput.value = ""
+  lobbyIdInput.setAttribute("disabled", "true")
+  lobbyInfo.textContent = JSON.stringify(lobby, null, 2)
 }
 
 function joinLobby(lobby) {
-  createLobbyBtn.setAttribute("disabled", "true");
-  joinLobbyBtn.setAttribute("disabled", "true");
-  exitLobbyBtn.style.pointerEvents = "all";
-  exitLobbyBtn.style.opacity = "1";
-  lobbyIdInput.value = "";
-  lobbyIdInput.setAttribute("disabled", "true");
-  lobbyInfo.textContent = JSON.stringify(lobby, null, 2);
+  createLobbyBtn.setAttribute("disabled", "true")
+  joinLobbyBtn.setAttribute("disabled", "true")
+  exitLobbyBtn.style.pointerEvents = "all"
+  exitLobbyBtn.style.opacity = "1"
+  lobbyIdInput.value = ""
+  lobbyIdInput.setAttribute("disabled", "true")
+  lobbyInfo.textContent = JSON.stringify(lobby, null, 2)
 }
 
 function updateLobbyInfo(lobby) {
-  currentLobby = lobby;
-  lobbyInfo.textContent = JSON.stringify(currentLobby, null, 2);
+  currentLobby = lobby
+  lobbyInfo.textContent = JSON.stringify(currentLobby, null, 2)
 }
 
 function displayLobbies(lobbies) {
   Object.entries(lobbies).forEach(([column, lobby]) => {
     lobbiesDiv.innerHTML += `
-      <button style="display:block;">${lobby.id}</button>
-    `;
-  });
+      <button style="display:block;" id="join-lobby-${lobby.id}">${lobby.id}</button>
+    `
+  })
 }
 
 function clearLobbiesList() {
-  lobbiesDiv.innerHTML = "";
+  lobbiesDiv.innerHTML = ""
 }
